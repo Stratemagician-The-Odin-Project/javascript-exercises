3befc45fa5dd148ae576c9a051d57f9b462338e2
Stratemagician <matthewjblair@protonmail.com>
2023-02-09 13:27:58 +1100

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let result = 0;
+
+    if (num1 > num2)
+    {
+        a = num2;
+        b = num1;
+    }
+    else
+    {
+        a = num1;
+        b = num2;
+    }
+
+    if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0)
+    {
+        for (let i = a; i <= b; i++)
+        {
+            result += i;
+        }
+        return result;
+    }
+
+    return "ERROR";
 };
 
 // Do not edit below this line
