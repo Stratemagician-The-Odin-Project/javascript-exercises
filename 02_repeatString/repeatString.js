806990ca5dc30b841cf32e63e273d35babaa941e
Stratemagician <matthewjblair@protonmail.com>
2023-02-09 12:12:29 +1100

    exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    let output = "";
+    
+    if (typeof s === "string" && n >= 0)
+    {
+        for (let i = 0; i < n; i++)
+        {
+            output += s;
+        }
+        return output;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
