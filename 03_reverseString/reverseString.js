7baa004baf2c56ad35b04ee943682dcdbb430eff
Stratemagician <matthewjblair@protonmail.com>
2023-02-09 13:19:25 +1100

    exercise 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let input = s.toString();
+    let output = "";
 
+    for (let i = input.length - 1; i >= 0; i--)
+    {
+        output += input.charAt(i);
+    }
+
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
 
+    var output = [];
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        let valid = false;
+
+        for (let j = 0; j < theArgs.length; j++)
+        {
+            if (arr[i] === theArgs[j])
+            {
+                valid = true;
+                break;
+            }
+        }
+
+        if (valid == false)
+        {
+            output.push(arr[i]);
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
