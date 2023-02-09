42551e94519ce0282274a7a39e1a5d3471a72693
Stratemagician <matthewjblair@protonmail.com>
2023-02-09 13:38:29 +1100

    exercise 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  if (typeof(temp) == "number")
+  {
+    return round((temp - 32) * 5 / 9, 1);
+  }
+  
+  return "Error";
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  if (typeof(temp) == "number")
+  {
+    return round((temp * 9 / 5) + 32, 1);
+  }
+  
+  return "Error";
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
